1f14c96de2625a16f19b8e78915872dc273cf5e4
Muhammad Luqman Asghar <luqmanposwal786@gmail.com>
2023-08-07 23:20:20 +0500

    assignment.js

--- assignment.js
+++ assignment.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("Jumma Mubarak");
+// aera and of rectangle
+var lenght = 23;
+var widht = 12;
+var aera = lenght * widht;
+console.log(aera);
+// volume of cube
+var lenght = 8;
+// because cube lenght=width=hight then
+var volume = lenght ** 3;
+console.log(volume);
+// cheknumber
+var num = -3;
+if (num > 0) {
+    console.log("positiv");
+}
+if (num == 0) {
+    console.log("0");
+}
+else {
+    console.log("negtive");
+}
+// number even/odd
+var num = 14;
+if (num / 2) {
+    console.log("Even");
+}
+else {
+    console.log("odd");
+}
+// check person is eligible for vote
+var age = 17;
+if (age >= 18) {
+    console.log("person is eligible for vote");
+}
+else {
+    console.log("person is not eligible for vote");
+}
+// math expression
+var math_Expression = ((10 + 5) * 3 - 2) / (4 % 3) - 7;
+console.log(math_Expression);
