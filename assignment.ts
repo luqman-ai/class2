ebc7b395238aba13bebab6e2360893f9bae1107c
Muhammad Luqman Asghar <luqmanposwal786@gmail.com>
2023-08-07 23:27:08 +0500

    assignment.ts

--- assignment.ts
+++ assignment.ts
@@ -1,1 +1,40 @@
-console.log ("Jumma Mubarak")
+// aera and of rectangle
+var lenght:number=23;
+var widht:number=12;
+var aera  :number=lenght*widht;
+console.log(aera);
+// volume of cube
+var lenght:number=8;
+// because cube lenght=width=hight then
+var volume:number=lenght**3;
+console.log(volume);
+// cheknumber
+var num:number = -3;
+    if (num>0) {
+        console.log("positiv");
+    }
+        if (num==0) {
+            console.log("0");
+           
+        } else {
+            console.log("negtive");
+        }
+    // number even/odd
+    var num=14
+    if (num/2) {
+        console.log("Even");
+        
+    } else {console.log("odd");        
+    }    
+    // check person is eligible for vote
+   var age=17
+    if (age>=18) {
+        console.log("person is eligible for vote");
+        
+    } else {console.log("person is not eligible for vote");
+        }
+    // math expression
+    var math_Expression:number=((10+5)*3-2)/(4%3)-7;
+    console.log(math_Expression);
+    
+
