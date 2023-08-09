fc95b412ad6aa1ecd8b31e0f228b4376d98cb89c
Muhammad Luqman Asghar <luqmanposwal786@gmail.com>
2023-08-09 07:01:13 +0500

    assignment

---
+++ assignment/task5.ts
@@ -0,0 +1,8 @@
+var age:number=27;
+if (age<=12){
+    console.log("Child");
+}
+else if(age>12&&age<=19){
+    console.log("Teenager");
+}
+else{ console.log("Adult")}
---
+++ assignment/task6.ts
@@ -0,0 +1,10 @@
+var temperature_C:number=18;
+if(temperature_C<=20){
+    console.log("Winter has come  user to wear Warm clothes")
+}
+else if(temperature_C<=30){
+    console.log("The temperatur is normal user wear normal clothes")
+}
+else{
+    console.log("Summer is here user aviod the Heat")
+}
